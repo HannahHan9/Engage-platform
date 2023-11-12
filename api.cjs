72e2730e017d4b50c55c978ae447c963e10f0541
HannahHan9 <h.han9204@gmail.com>
2023-11-12 18:57:11 +0000

    add hosted server

--- api.cjs
+++ api.cjs
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 app.use(cors());
 
-export const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on ${port}`));
 
